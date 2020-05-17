7ca56cd0bfea8590969a8b050b107afd8b69ead3
tramanhk11uit <tramanh1998tls@gmail.com>
2020-05-18 01:34:11 +0700

    delete

--- FrontEnd/src/helper/if.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export default function If({ component: Component, condition, props }) {
-    if (condition)
-        return <Component {...props} />
-    return null;
-}
